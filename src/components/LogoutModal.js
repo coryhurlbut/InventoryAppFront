e544de7a9790045b11caea3da60c762c99985551
CoryHurlbut <cory.hurlbut@us.af.mil>
2021-08-05 09:10:14 -0500

    Added files that did not get commited in last push.

---
+++ src/components/LogoutModal.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Modal} from '@fluentui/react';
+import {loginLogoutController} from '../controllers/LoginLogoutController';
+
+/*
+*   Modal for logging out
+*/
+export default class LogoutModal extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen:     props.isOpen,
+        };
+
+        this.logout         = this.logout.bind(this);
+        this.dismissModal   = this.dismissModal.bind(this);
+    };
+
+    async logout() {
+        await loginLogoutController.logout(this.props.auth).then(() => this.props.clearAuth())
+        this.dismissModal();
+    };
+
+    dismissModal() {
+        this.setState({isOpen: false});
+    };
+
+    render() {
+        return(
+            <Modal isOpen={this.state.isOpen} onDismissed={this.props.hideModal}>
+                <div>
+                    <div className='header'>
+                        Log Out
+                    </div>
+                    <button onClick={this.logout}>Log Out</button>
+                    <button onClick={this.dismissModal}>Close</button>
+                </div>
+            </Modal>
+        );
+    };
+};
---
+++ src/controllers/LoginLogoutController.js
@@ -0,0 +1,35 @@
+import GenericController from './GenericController';
+import AuthController from './AuthController';
+/*
+*   Controls functions calling APIs for Auth data operations
+*/
+export default class LoginLogoutController extends GenericController{
+    
+    async logout(auth) {
+        localStorage.clear();
+        return await this.request('auth/logout', {method: 'DELETE', body: JSON.stringify(auth)});
+    };
+
+    //Logs in user. Must pass userName and password
+    async login(userName, password) {
+        const reqBody = {
+                userName: userName,
+                password: password
+        };
+
+        const reqObj = {
+            method: 'POST',
+            body: JSON.stringify(reqBody)
+        };
+        
+        let auth = await this.request('auth/login', reqObj).then((res) => res.json());
+        localStorage.clear();
+        localStorage.setItem('access', auth.accessToken);
+        localStorage.setItem('refresh', auth.refreshToken);
+
+        return auth
+    };
+};
+
+//Exports an instance of the class instead of the class
+export const loginLogoutController = new LoginLogoutController();
