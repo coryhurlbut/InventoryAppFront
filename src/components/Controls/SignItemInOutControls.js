ad5cbc9c7315c4efbca1b0327093e8cc34e6923d
CoryHurlbut <cory.hurlbut@us.af.mil>
2022-02-10 10:13:23 -0600

    Forgot to add untracked files.

---
+++ src/components/Controls/SignItemInOutControls.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import '@fluentui/react';
+import {SignItemInModal, SignItemOutModal} from '../ItemModals';
+
+/*
+*   Displays the button to sign an item in or out. Dependent on data displayed in the table.
+*/
+export default class SignItemInOutControls extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            signItemInOutIsVisible: props.signItemInOutIsVisible,
+            modal: null,
+            id: props.id,
+            idArray: props.idArray
+        };
+
+        this.hideModal      = this.hideModal.bind(this);
+        this.signItemIn     = this.signItemIn.bind(this);
+        this.signItemOut    = this.signItemOut.bind(this);
+        this.buildButton    = this.buildButton.bind(this);
+
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.signItemInOutIsVisible !== prevProps.signItemInOutIsVisible) {
+            this.setState({ signItemInOutIsVisible: this.props.signItemInOutIsVisible });
+        };
+        if(prevProps !== this.props){
+            this.setState({ idArray: this.props.idArray })
+        }
+    };
+
+    hideModal() {
+        this.setState({modal: null});
+    };
+
+    signItemIn() {
+        this.setState({modal: <SignItemInModal isOpen={true} idArray={this.state.idArray} hideModal={this.hideModal}/>})
+    };
+
+    signItemOut() {
+        this.setState({modal: <SignItemOutModal isOpen={true} idArray={this.state.idArray} hideModal={this.hideModal}/>})
+    };
+
+    buildButton() {
+        if (this.props.inOrOut === 'Sign Item In') {
+            return <button onClick={this.signItemIn} disabled={this.state.idArray.length > 0 ? false : true}>{this.props.inOrOut}</button>
+        } else if (this.props.inOrOut === 'Sign Item Out') {
+            return <button onClick={this.signItemOut} disabled={this.state.idArray.length > 0 ? false : true}>{this.props.inOrOut}</button>
+        } else {
+            return null;
+        };
+    };
+
+    render() {
+        return(
+            <div>
+                {this.state.modal}
+                {this.state.signItemInOutIsVisible ? this.buildButton(): null}
+            </div>
+        );
+    };
+};
---
+++ src/components/Controls/UserEditControls.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import '@fluentui/react';
+// import UserController from '../controllers/UserController';
+import { AddUserModal, DeleteUserModal, EditUserModal } from '..//UserModals';
+
+/*
+*   Displays the buttons for adding, deleting and editing users
+*/
+export default class UserEditControls extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            modal: null,
+            id: props.id,
+            idArray: props.idArray
+        };
+
+        this.hideModal  = this.hideModal.bind(this);
+        this.addUser    = this.addUser.bind(this);
+        this.editUser   = this.editUser.bind(this);
+        this.deleteUser = this.deleteUser.bind(this);
+    };
+
+    componentDidUpdate(prevProps, prevState){
+        if(prevProps !== this.props){
+            this.setState({ id: this.props.id, idArray: this.props.idArray});
+        };
+    };
+    
+    addUser () {
+        this.setState({modal: <AddUserModal isOpen={true} hideModal={this.hideModal}/>});
+    };
+
+    editUser () {
+        this.setState({modal: <EditUserModal isOpen={true} id={this.state.id} hideModal={this.hideModal}/>});
+    };
+
+    deleteUser () {
+        this.setState({modal: <DeleteUserModal isOpen={true} id={this.state.id} hideModal={this.hideModal} idArray={this.state.idArray}/>});
+    };
+
+    hideModal() {
+        this.setState({modal: null});
+    };
+
+    render() {
+        return(
+            <div>
+                <button onClick={this.addUser}>
+                    Add User
+                </button>
+                <button onClick={this.editUser} disabled={this.state.idArray.length === 1 ? false : true}>
+                    Edit User
+                </button>
+                <button onClick={this.deleteUser} disabled={this.state.idArray.length > 0 ? false : true}>
+                    Delete User
+                </button>
+                {this.state.modal}
+            </div>
+        );
+    };
+};
