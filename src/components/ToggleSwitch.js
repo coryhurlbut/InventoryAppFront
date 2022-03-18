1d3cb148381e1de084659d0c41b17a7070122e1e
sumerau <jessie@sumerau.com>
2022-03-18 14:16:23 -0500

    Styling: nonfunctional dark mode toggle

---
+++ src/components/ToggleSwitch.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles/ToggleSwitch.css";
+
+const ToggleSwitch = () => {
+return (
+	<div className="toggleSwitchContainer">
+        <div className="toggleSwitch">
+            <input 
+                type="checkbox" 
+                className="checkbox"
+                name='lightDark' 
+                id='lightDark' />
+            <label className="label" htmlFor='lightDark'>
+            <span className="inner" />
+            <span className="switch" />
+            </label>
+        </div>
+	</div>
+);
+};
+
+export default ToggleSwitch;
